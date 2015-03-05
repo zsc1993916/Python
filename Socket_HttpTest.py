9e508e8b24310253048e74d7f2ef8bfc97d164d4
Zo Zhou <565297712@qq.com>
2015-03-05 21:25:53 +0800

    Create Socket_HttpTest.py
    创建socket，向百度发送http请求，接收服务器返回的数据

---
+++ Socket_HttpTest.py
@@ -0,0 +1,27 @@
+# 导入socket库
+import socket
+# 创建socket
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# 建立连接
+s.connect(('www.baidu.com', 80))
+# 发送一个HTTP请求
+s.send('GET / HTTP/1.1\r\nHost: www.baidu.com\r\nConnection: close\r\n\r\n')
+# 接收数据:
+buffer = []
+while True:
+    # 每次最多接收1k字节:
+    d = s.recv(1024)
+    if d:
+        buffer.append(d)
+    else:
+        break
+data = ''.join(buffer)
+# 关闭连接:
+s.close()
+#分离http头和html
+header, html = data.split('\r\n\r\n', 1)
+print ("header:\n"),(header)
+
+# 把接收的数据写入文件:
+with open('baidu.html', 'wb') as f:
+    f.write(html)
